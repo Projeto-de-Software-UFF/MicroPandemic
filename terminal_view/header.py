eaeb0c02df8f723af10217e0f07338a9b42e1283
Disklo <rafaellcarvalho5@gmail.com>
2025-07-01 13:29:45 -0300

    Delete terminal_view directory

--- terminal_view/header.py
+++
@@ -1,5 +0,0 @@
-def header(turno = 0):
-    print('=========================')
-    print('===== MicroPandemic =====')
-    print('=========================')
-    print(f'Turno do Jogador: {turno}')
--- terminal_view/menu.py
+++
@@ -1,9 +0,0 @@
-def menu():
-    print('\nSelecione uma ação no menu abaixo:')
-    print(
-        """
-        1. Usar uma carta da mão
-        2. Mover-se para uma cidade vizinha
-        3. Passar (pular ações restantes)
-        0. Sair do Jogo
-    """)
