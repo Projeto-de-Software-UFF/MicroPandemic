6cbb8277a48ad3339e386ec23258beae20777d5a
Disklo <rafaellcarvalho5@gmail.com>
2025-07-01 14:12:29 -0300

    Fix: Player draws a card after using 'Compartilhar Carta'.

--- domain/carta/compartilhar_carta.py
+++ domain/carta/compartilhar_carta.py
@@ -11,4 +11,8 @@ class CompartilharCarta(Carta):
 
     def ativar(self, jogo: 'Jogo', jogador: 'Jogador', outro_jogador: 'Jogador', carta_a_compartilhar: 'Carta') -> bool:
         print(f"{jogador.nome} usando Compartilhar Carta com {outro_jogador.nome}.")
-        return jogador.compartilhar_carta(outro_jogador, carta_a_compartilhar)
+        if jogador.compartilhar_carta(outro_jogador, carta_a_compartilhar):
+            print(f"{jogador.nome} comprou uma carta após compartilhar.")
+            jogo.baralho.comprar_carta()
+            return True
+        return False
